c439dcdd4100f6c533417279aa2f224ac9756b74
screen <bao.future94@gmail.com>
2020-08-27 17:24:07 -0700

    aaa

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionalClick from './components/FuntionalClick';
-import CLassClick from './components/ClassCLick';
-import EventBinding from './components/EventBinding';
-import ChildComponent from './components/ChildComponent';
-import ParentComponent from './components/ParentComponent';
-import UserGreet from './components/UserGreet';
-import NameList from './components/NameList';
-import StyleSheet from './components/StyleSheet';
-import Inline from './components/Inline';
 import './components/appStyle.css';
-import style from './components/appStyle.module.css';
-import Form from './components/Form';
-import LifeCycleA from './components/LifeCycleA';
-import ClassCounter from './components/hook/ClassCounter';
-import HookCounter from './components/hook/HookCounter';
-import HookCounterThree from './components/hook/HookCounterThree';
-import HookCounterFour from './components/hook/HookCounterFour';
-import HookCounterTwo from './components/hook/HookCounterTwo';
+import ClassCounterOne from './components/hook/ClassCounterOne';
+import HookCounterOne from './components/hook/HookCounterOne';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterFour />
+      <ClassCounterOne />
+      {/* <HookCounterOne /> */}
+      {/* <HookCounterFour /> */}
       {/* <HookCounterThree /> */}
       {/* <HookCounterTwo /> */}
       {/* <HookCounter />
--- src/components/LifeCycleA.js
+++ src/components/LifeCycleA.js
@@ -4,27 +4,49 @@ class LifeCycleA extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: 'Bao'
-        }
+            count: 2,
+            name: ', This is State'
+        };
 
-        console.log('Lifecycle A constructor')
     }
 
-    static getDerivedStateFromProps(props, state) {
-        console.log('Lifecycle A getDerivedStateFromProps');
-        return null
+    componentWillMount() {
+        console.log('1- component will mount');
     }
 
     componentDidMount() {
-        console.log('LifeCycle A componentDidmount')
+        console.log('3 - component did mount \r\n waiting 2s ...');
+    }
+
+    componentWillReceiveProps(nextProp) {
+        this.setState({ count: 7, name: ', This is new State' });
+        console.log('4- component will receive prop: ' + nextProp.name);
+    }
+
+    shouldComponentUpdate(nextProp, nextState) {
+        if (this.state.name !== nextState.name) {
+            console.log('5- should component update: ' + nextProp.name + ' ' + nextState.name);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    componentWillUpdate(nextProp, nextState) {
+        console.log('6- component will update: ' + nextProp.name + ' ' + nextState.name);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('8- OK component did update: ' + prevProps.name + ' ' + prevState.name);
+    }
+
+    componentWillUnmount() {
+        console.log('9- component has removed');
     }
     render() {
-        console.log('LifeCycle A render')
-        return (
-            <div>
-                
-            </div>
-        )
+        console.log(this.state.count + '- render DOM');
+        return <div>{this.props.name}</div>;
     }
 }
 
